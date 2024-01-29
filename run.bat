4a74d3d5ccdf9b91a214a67bcfe0da7a6d6a268f
CommanderKV <kylervisser@gmail.com>
2024-01-29 14:46:13 -0500

    First commit

---
+++ run.bat
@@ -0,0 +1,17 @@
+@echo off
+git add -A
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "fullstamp=%YY%/%MM%/%DD% %HH%:%Min%:%Sec%"
+git commit -a -m "Updated on: %fullstamp%"
+git remote add origin "https://github.com/CommanderKV/Java-COMP1008G-WAB.git"
+
+
+echo ------------------------------------------------------------
+git push
+echo ------------------------------------------------------------
+ 
+pause
---
+++ week-4/Memory-Card-Game/src/main/java/com/example/memorycardgame/MemoryGameController.java
@@ -0,0 +1,22 @@
+package com.example.memorycardgame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+
+public class MemoryGameController {
+
+    @FXML
+    private Label CorrectGuessesLabel;
+
+    @FXML
+    private Label GuessesLabel;
+
+    @FXML
+    private FlowPane cardFlowPane;
+
+    @FXML
+    private Button resetButton;
+
+}
